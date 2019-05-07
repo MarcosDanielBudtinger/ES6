089111574862ee4e809f28cd2248b8e527be4f97
Marcos Daniel <marcos.mdbbr@gmail.com>
2019-05-07 17:16:47 -0300

    ES6 template string

---
+++ template_string.js
@@ -0,0 +1,15 @@
+//com template string não tem mais a necessidade de concatenar elementos :D
+
+
+let nome = 'Marcos Daniel Budtinger';
+
+let html = `
+   
+    <h3>ES6 Teplate String<\h3>
+
+    <p>Utilizando template String<\p>
+
+    <p>Não precisa utilizar o + com as '' para 
+        adicionar outros elementos.</p>
+`;
+
