70831960b1d95afc849a2c89716e871f4f8a730c
Marcos Daniel <marcos.mdbbr@gmail.com>
2019-05-09 09:37:39 -0300

    Test loops with array and object, how make for go throught a object

---
+++ for_of_loops.js
@@ -0,0 +1,43 @@
+let array = [1,2,3,4,5,6];
+
+for(let i in array){
+    console.log(i, array[i]);
+}
+
+let obj = {
+    first_name: 'Marcos',
+    last_name: 'Daniel'
+};
+
+for(let i in obj){
+    console.log(obj);
+    //console.log(i, obj[i]);
+    //console
+}
+
+//with array more easy work with of
+for(let valeu of array){
+    console.log(value);
+}
+
+//example array the objects
+let newArray = [
+    {name: 'Marcos', age: 29}, {name: 'João', age: 30}, 
+    {name: 'Maria', age: 19}, {name: 'Carlos', age:21}
+];
+
+for(let person of newArray){
+    console.log(person);
+    //console.log(obj);
+}
+
+//case need the index
+for(let person of newArray.entries()){
+    console.log(person);
+}
+
+let keys = Object.keys(obj);
+for(let key of keys){
+    console.log(obj[key]);
+}
+
